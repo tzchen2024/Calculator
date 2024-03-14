2176cfb55e669adffe83508b9c05ab7c938dcec4
tzchen2024 <160681588+tzchen2024@users.noreply.github.com>
2024-03-14 12:22:30 -0700

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,46 @@
+quiz = {
+  #First, we create a variable called quiz.
+  #Inside quiz, we have several questions numbered from 1 to 7, each represented as a dictionary.
+  "question1": {
+    "question": "What is the capital of France?",
+    "answer": "Paris"},
+  "question2": {
+    "  question": "What is the capital of Germany?",
+    "answer": "Berlin"},
+  "question3": {
+      "question": "What is the capital of Italy?",
+    "answer": "Rome"},
+  "question4": {
+    "question": "What is the capital of Spain?",
+  "answer": "Madrid"},
+  "question5": {
+    "question": "What is the capital of Portugal?",
+  "answer": "Lisbon"},
+  "question6": {
+    "question": "What is the capital of Switzerland?",
+  "answer": "Bern"},
+  "question7": {
+    "question": "What is the capital of Austria?",
+  "answer": "Vienna"},
+  }
+score = 0
+#We set up a variable called score and start it at 0. This will keep track of how many questions the player answers correctly.
+for key, value in quiz.items():
+  print(value['question'])
+  answer = input("Answer? ")
+  if answer.lower() == value[ 'answer'] . lower():
+  #This line of code is checking if the answer provided by the user (stored in the variable answer) matches the correct answer stored in a dictionary (value['answer']), regardless of capitalization. If the lowercase version of the user's answer matches the lowercase version of the correct answer, the condition is true.
+    print('Correct')
+    score = score + 1
+    print("Your score is " + str(score))
+    print("")
+    print("")
+  else:
+    print("Wrong!")
+    print("The right answer is " + value[ "answer"])
+    print("Your score is " + str(score))
+    print("")
+    print("")
+  
+print("You got " + str(score) + "out of 7 questions correctly")
+print("Your percentage is " + str(int(score/7*100))+"%")
